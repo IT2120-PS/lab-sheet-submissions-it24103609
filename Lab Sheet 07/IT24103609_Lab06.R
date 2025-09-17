d3aa5b1e3348068e6904c2e919ea26657ac53017
it24103609 <it24103609@my.sliit.lk>
2025-09-17 20:20:00 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24103609_Lab06.R
@@ -0,0 +1,13 @@
+setwd("C:\\Users\\it24100248\\Desktop\\Lab 6")
+getwd()
+
+#1)
+#i. Binomial distribution
+#ii.
+1-pbinom(46,50,0.85,lower.tail=TRUE)
+
+#2)
+#i. Number of calls per hour
+#ii. Possion distribution.
+#iii. 
+dpois(15,12)
