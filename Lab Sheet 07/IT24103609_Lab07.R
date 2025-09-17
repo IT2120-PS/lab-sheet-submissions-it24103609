40ff9db2611873457622db6825097e4fb0d2f863
it24103609 <it24103609@my.sliit.lk>
2025-09-17 20:25:03 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24103609_Lab07.R
@@ -0,0 +1,19 @@
+setwd('C:\\Users\\USER\\Desktop\\IT24100383')
+getwd()
+#Exercise
+#01)
+
+punif(25,min = 0,max = 40, lower.tail = TRUE) 
+- punif(10,min = 0,max = 40, lower.tail = TRUE)
+
+#02)
+
+pexp(2,rate = 0.33,lower.tail = TRUE)
+
+#03)
+#i)
+1 -  pnorm(130, mean = 100, sd = 15,lower.tail = TRUE)
+
+#ii)
+qnorm(0.95, mean = 100, sd = 15,lower.tail = TRUE)
+
